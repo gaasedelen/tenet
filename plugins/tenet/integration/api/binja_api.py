4dd2268b7b870ac5cb92833d07f5b598c5041640
Etch <etch@debugmen.dev>
2022-11-08 00:56:29 -0500

    A little more progress

--- plugins/tenet/integration/api/binja_api.py
+++ plugins/tenet/integration/api/binja_api.py
@@ -4,6 +4,8 @@ import functools
 import binaryninja
 import binaryninja.mainthread as mainthread
 import binaryninja.binaryview as binaryview
+import binaryninja.highlevelil as highlevelil
+import binaryninja.architecture as architecture
 
 
 from .api import DisassemblerCoreAPI, DisassemblerContextAPI
@@ -118,7 +120,7 @@ class BinjaCoreAPI(DisassemblerCoreAPI):
     #--------------------------------------------------------------------------
     # UI API Shims
     #--------------------------------------------------------------------------
-    #TODO This thing. UI in binja seems to suck
+    #TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     def create_dockable(self, window_title, widget):
 
         # create a dockable widget, and save a reference to it for later use
@@ -145,3 +147,111 @@ class BinjaContextAPI(DisassemblerContextAPI):
     @property
     def busy(self):
         return binaryview.AnalysisInfo.state != binaryview.AnalysisState.IdleState
+
+    #--------------------------------------------------------------------------
+    # API Shims
+    #--------------------------------------------------------------------------
+
+    @BinjaCoreAPI.execute_read
+    def get_current_address(self) -> int:
+        return highlevelil.HighLevelILFunction.current_address
+
+    def get_processor_type(self):
+        ## get the target arch, PLFM_386, PLFM_ARM, etc # TODO
+        #arch = idaapi.ph_get_id()
+        pass
+
+    def is_64bit(self) -> bool:
+        return binaryview.address_size & 8
+
+    def is_call_insn(self, address):
+        functions = binaryview.get_functions_containing(address)
+        if functions[0].is_call_instruction(address):
+            return True
+        return False
+
+    def get_instruction_addresses(self) -> list:
+        """
+        Return all instruction addresses from the executable.
+        """
+        instruction_addresses = []
+
+        # for section in binaryview.section:
+
+        #     # fetch executable segments
+        #     #! Dont know if binja can do "Code" segments
+        #     if not section.semantics.ReadOnlyCodeSectionSemantics:
+        #         continue
+
+        # Iterate through disassembly and check if is valid instruction
+        disassembly_generator = binaryview.get_linear_disassembly()
+        for line in disassembly_generator:
+            if line.contents.il_instruction:
+                instruction_addresses.append(line.contents.address)
+
+        return instruction_addresses
+
+    #! Not sure how binja will deal with heap addresses and stuff
+    def is_mapped(self, address):
+        mapped = False
+        for seg in binaryview.segments:
+            if seg.start < address < seg.end:
+                mapped = True
+                break
+        return mapped
+
+    #TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    def get_next_insn(self, address):
+
+        xb = ida_xref.xrefblk_t()
+        ok = xb.first_from(address, ida_xref.XREF_ALL)
+
+        while ok and xb.iscode:
+            if xb.type == ida_xref.fl_F:
+                return xb.to
+            ok = xb.next_from()
+
+        return -1
+    #TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    def get_prev_insn(self, address):
+
+        xb = ida_xref.xrefblk_t()
+        ok = xb.first_to(address, ida_xref.XREF_ALL)
+
+        while ok and xb.iscode:
+            if xb.type == ida_xref.fl_F:
+                return xb.frm
+            ok = xb.next_to()
+
+        return -1
+
+    #? Fullpath? Or just the final directory
+    def get_database_directory(self):
+        return binaryview.file.filename.split("/")[-2]
+
+    def get_function_addresses(self) -> list:
+        addresses = []
+        functions = binaryview.functions
+        for function in functions:
+            addresses.append(function.address_ranges[0].start)
+        return addresses
+    
+    #? I guess there is a difference in IDA? Neither of these are even used
+    #? so whatever
+    def get_function_name_at(self, address) -> str:
+        # Assume 1 functino
+        functions = binaryview.get_functions_containing(address)
+        return functions[0].name
+
+    def get_function_raw_name_at(self, function_address) -> str:
+        # Assume 1 functino
+        functions = binaryview.get_functions_containing(function_address)
+        return functions[0].name
+
+
+    def get_imagebase(self) -> int:
+        #? Does binja really not have a base address api call
+        return binaryview.segments[0].start
+
+    def get_root_filename(self) -> str:
+        return binaryview.file.filename.split("/")[-1]
